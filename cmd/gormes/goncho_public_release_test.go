52310b235decf08ebd1c88f0e59d813843f54305
xel <tamez.jm@gmail.com>
2026-05-20 18:23:01 -0600

    test: guard Goncho public release import

--- cmd/gormes/goncho_public_release_test.go
+++ cmd/gormes/goncho_public_release_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"testing"
@@ -9,6 +12,35 @@ import (
 	gormesgoncho "github.com/TrebuchetDynamics/goncho/integration/gormes"
 	"github.com/TrebuchetDynamics/gormes-agent/internal/tools"
 )
+
+func TestGormesGonchoDependencyUsesLatestPublicRelease(t *testing.T) {
+	cmd := exec.Command("go", "list", "-m", "-json", "github.com/TrebuchetDynamics/goncho")
+	cmd.Env = append(os.Environ(), "GOWORK=off")
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("go list goncho module: %v\n%s", err, out)
+	}
+	var mod struct {
+		Path    string
+		Version string
+		Replace *struct {
+			Path    string
+			Version string
+		}
+	}
+	if err := json.Unmarshal(out, &mod); err != nil {
+		t.Fatalf("parse go list module JSON: %v\n%s", err, out)
+	}
+	if mod.Path != "github.com/TrebuchetDynamics/goncho" {
+		t.Fatalf("module path = %q, want github.com/TrebuchetDynamics/goncho", mod.Path)
+	}
+	if mod.Replace != nil {
+		t.Fatalf("goncho dependency is replaced by %s %s, want latest public release", mod.Replace.Path, mod.Replace.Version)
+	}
+	if mod.Version != "v0.1.0" {
+		t.Fatalf("goncho version = %q, want latest public release v0.1.0", mod.Version)
+	}
+}
 
 func TestGormesGonchoPublicRuntimeRegistersToolsAndReportsStatus(t *testing.T) {
 	ctx := context.Background()
